faa94f08f0f636454999e65370399bccd8d7e814
lieselotte11 <GEwigkeitm107_50Cal112358_mk23@yahoo.co.jp>
2025-09-26 14:54:46 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/1755789580462/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xa2, 0x9b, 0x79, 0x0b, 0xf7, 0xfd, 0x73, 0x37}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
